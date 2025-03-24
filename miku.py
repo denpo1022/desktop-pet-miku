ab3d7c652b9473e7c215b933fd69c91c5afc36f0
denpo1022 <mario159357@gmail.com>
2025-03-24 22:55:53 +0800

    Format

--- miku.py
+++ miku.py
@@ -7,14 +7,15 @@ from PyQt5.QtGui import *
 from PyQt5.QtCore import Qt, QTimer
 from PyQt5 import QtWidgets, QtGui
 
+
 class MyMikuPet(QWidget):
     def __init__(self):
         QtWidgets.QWidget.__init__(self)
         quit = QAction("退出", self, triggered=os._exit)
         quit.setIcon(QIcon("img/icon.png"))
-        addPet = QAction("添加一个Miku", self, triggered = addOnePet)
+        addPet = QAction("添加一个Miku", self, triggered=addOnePet)
         addPet.setIcon(QIcon("img/icon.png"))
-        removePet = QAction("移除一个Miku", self, triggered = delOnePet)
+        removePet = QAction("移除一个Miku", self, triggered=delOnePet)
         removePet.setIcon(QIcon("img/icon.png"))
         about = QAction("About", self, triggered=aboutInfo)
         about.setIcon(QIcon("img/icon.png"))
@@ -29,24 +30,30 @@ class MyMikuPet(QWidget):
         self.trayIcon.setContextMenu(self.trayIconMenu)
         self.trayIcon.show()
 
+
 def addOnePet():
     pets.append(myPet())
 
+
 def delOnePet():
-    if len(pets)==0:
+    if len(pets) == 0:
         return
-    del pets[len(pets)-1]
+    del pets[len(pets) - 1]
+
 
 def aboutInfo():
     webbrowser.open_new_tab("https://github.com/luxingwen/desktop-pet-miku")
 
+
 class myPet(QWidget):
-    def __init__(self, parent = None):
+    def __init__(self, parent=None):
         QtWidgets.QWidget.__init__(self)
         self.initUI()
 
     def initUI(self):
-        self.setWindowFlags(Qt.FramelessWindowHint|Qt.WindowStaysOnTopHint|Qt.SubWindow)
+        self.setWindowFlags(
+            Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint | Qt.SubWindow
+        )
         self.setAttribute(Qt.WA_TranslucentBackground)
         self.repaint()
         self.img = QLabel(self)
@@ -66,44 +73,104 @@ class myPet(QWidget):
         listPic = []
         for item in pics:
             img = QImage()
-            img.load('img/'+item)
+            img.load("img/" + item)
             listPic.append(img)
         return listPic
 
     def initData(self):
         imgs = self.getImgs(["shime1b.png", "shime2b.png", "shime1b.png", "shime3b"])
         self.actionDatas.append(imgs)
-        imgs = self.getImgs(["shime11.png", "shime15.png", "shime16.png", "shime17.png", "shime16.png", "shime17.png", "shime16.png", "shime17.png"])
+        imgs = self.getImgs(
+            [
+                "shime11.png",
+                "shime15.png",
+                "shime16.png",
+                "shime17.png",
+                "shime16.png",
+                "shime17.png",
+                "shime16.png",
+                "shime17.png",
+            ]
+        )
         self.actionDatas.append(imgs)
-        imgs = self.getImgs(["shime54.png", "shime55.png", "shime26.png", "shime27.png", "shime28.png", "shime29.png","shime26.png", "shime27.png", "shime28.png", "shime29.png","shime26.png", "shime27.png", "shime28.png", "shime29.png"])
+        imgs = self.getImgs(
+            [
+                "shime54.png",
+                "shime55.png",
+                "shime26.png",
+                "shime27.png",
+                "shime28.png",
+                "shime29.png",
+                "shime26.png",
+                "shime27.png",
+                "shime28.png",
+                "shime29.png",
+                "shime26.png",
+                "shime27.png",
+                "shime28.png",
+                "shime29.png",
+            ]
+        )
         self.actionDatas.append(imgs)
-        imgs = self.getImgs(["shime31.png", "shime32.png", "shime31.png", "shime33.png"])
+        imgs = self.getImgs(
+            ["shime31.png", "shime32.png", "shime31.png", "shime33.png"]
+        )
         self.actionDatas.append(imgs)
         imgs = self.getImgs(["shime18.png", "shime19.png"])
         self.actionDatas.append(imgs)
-        imgs = self.getImgs(["shime34b.png", "shime35b.png", "shime34b.png", "shime36b.png"])
+        imgs = self.getImgs(
+            ["shime34b.png", "shime35b.png", "shime34b.png", "shime36b.png"]
+        )
         self.actionDatas.append(imgs)
-        imgs = self.getImgs(["shime14.png", "shime14.png", "shime52.png", "shime13.png", "shime13.png", "shime13.png", "shime52.png", "shime14.png"])
+        imgs = self.getImgs(
+            [
+                "shime14.png",
+                "shime14.png",
+                "shime52.png",
+                "shime13.png",
+                "shime13.png",
+                "shime13.png",
+                "shime52.png",
+                "shime14.png",
+            ]
+        )
         self.actionDatas.append(imgs)
-        imgs = self.getImgs(["shime42.png", "shime43.png", "shime44.png", "shime45.png", "shime46.png"])
+        imgs = self.getImgs(
+            ["shime42.png", "shime43.png", "shime44.png", "shime45.png", "shime46.png"]
+        )
         self.actionDatas.append(imgs)
-        imgs = self.getImgs(["shime1.png", "shime38.png", "shime39.png", "shime40.png", "shime41.png"])
+        imgs = self.getImgs(
+            ["shime1.png", "shime38.png", "shime39.png", "shime40.png", "shime41.png"]
+        )
         self.actionDatas.append(imgs)
-        imgs = self.getImgs(["shime25.png", "shime25.png", "shime53.png", "shime24.png", "shime24.png", "shime24.png", "shime53.png", "shime25.png"])
+        imgs = self.getImgs(
+            [
+                "shime25.png",
+                "shime25.png",
+                "shime53.png",
+                "shime24.png",
+                "shime24.png",
+                "shime24.png",
+                "shime53.png",
+                "shime25.png",
+            ]
+        )
         self.actionDatas.append(imgs)
-        imgs = self.getImgs(["shime20.png", "shime21.png", "shime20.png", "shime21.png", "shime20.png"])
+        imgs = self.getImgs(
+            ["shime20.png", "shime21.png", "shime20.png", "shime21.png", "shime20.png"]
+        )
         self.actionDatas.append(imgs)
 
     def actionRun(self):
         if not self.runing:
-            self.action = random.randint(0, len(self.actionDatas)-1)
+            self.action = random.randint(0, len(self.actionDatas) - 1)
             self.index = 0
             self.runing = True
         self.runFunc(self.actionDatas[self.action])
 
     def setPic(self, pic):
         img = QImage()
-        img.load('img/'+pic)
+        img.load("img/" + pic)
         self.img.setPixmap(QPixmap.fromImage(img))
 
     def runFunc(self, imgs):
@@ -115,26 +182,32 @@ class myPet(QWidget):
 
     def randomPos(self):
         screen = QDesktopWidget().screenGeometry()
-        size =  self.geometry()
-        self.move(int((screen.width()-size.width())*random.random()), int((screen.height()-size.height())*random.random()))
+        size = self.geometry()
+        self.move(
+            int((screen.width() - size.width()) * random.random()),
+            int((screen.height() - size.height()) * random.random()),
+        )
 
     def mousePressEvent(self, event):
-        if event.button()==Qt.LeftButton:
-            self.m_drag=True
-            self.m_DragPosition=event.globalPos()-self.pos()
+        if event.button() == Qt.LeftButton:
+            self.m_drag = True
+            self.m_DragPosition = event.globalPos() - self.pos()
             event.accept()
             self.setCursor(QCursor(Qt.OpenHandCursor))
+
     def mouseMoveEvent(self, QMouseEvent):
         if Qt.LeftButton and self.m_drag:
-            self.move(QMouseEvent.globalPos()-self.m_DragPosition)
+            self.move(QMouseEvent.globalPos() - self.m_DragPosition)
             QMouseEvent.accept()
+
     def mouseReleaseEvent(self, QMouseEvent):
-        self.m_drag=False
+        self.m_drag = False
         self.setCursor(QCursor(Qt.ArrowCursor))
+
 
 if __name__ == "__main__":
     global pets
-    pets=[]
+    pets = []
     app = QApplication(sys.argv)
     w = MyMikuPet()
     sys.exit(app.exec_())
